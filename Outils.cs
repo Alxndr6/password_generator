008f1fc9f8abba432144ae25bb4a83eb17198f9f
Alexander AULONG <133575651+Alxndr6@users.noreply.github.com>
2023-06-07 14:38:09 +0200

    Add files via upload

---
+++ Outils.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generateur_mot_de_passe
+{
+    public static class Outils
+    {
+        static int DemanderNombre(string question)
+        {
+            int longueurInt = 0;
+
+            while (true)
+            {
+                Console.Write(question);
+                string reponse = Console.ReadLine();
+
+                try
+                {
+                    longueurInt = int.Parse(reponse);
+                    return longueurInt;
+                }
+                catch
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("ERREUR : Vous devez entrer un nombre.");
+                }
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+        }
+
+        public static int DemanderNombreEntre(string question, int min, int max, string messageErreurPersonnalise = null)
+        {
+            int nombre = DemanderNombre(question);
+
+            if ((nombre >= min) && (nombre <= max))
+            {
+                return nombre;
+            }
+            if(messageErreurPersonnalise == null)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine($"ERREUR : Le nombre doit être compris entre {min} et {max}.");
+
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(messageErreurPersonnalise);
+
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+            return DemanderNombreEntre(question, min, max, messageErreurPersonnalise);
+        }
+
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue, "ERREUR : Le nombre doit être positif et non nul.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace generateur_mot_de_passe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NOMBRE_MDP = 10; // Définit le nombre de mots de passe générés avec les paramètres choisis par l'utilisateur.
+
+            string minuscules = "abcdefghijklmnopqrstuvwxyz";
+            string majuscules = minuscules.ToUpper();
+            string chiffres = "0123456789";
+            string caracteresSpeciaux = "!?&@/-_~";
+            string alphabet; // Contiendra les différentes parties concaténées selon le choix de l'utilisateur via "int choixAlphabet".
+            string motDePasse = "";
+            Random rand = new Random();
+
+            int longueurMotDePasse = Outils.DemanderNombrePositifNonNul("Longueur de mot de passe : ");
+            Console.WriteLine();
+            int choixAlphabet = Outils.DemanderNombreEntre("1 - Uniquement des caractères en minuscule\n" + "2 - Des caractères minuscules et majuscules\n" +
+                "3 - Des caractères et des chiffres\n" + "4 - Caractères, chiffres et caractères spéciaux\n" + "Entrez votre choix : ", 1, 4);
+
+            if (choixAlphabet == 1 )
+            {
+                alphabet = minuscules;
+            }
+            else if (choixAlphabet == 2 )
+            {
+                alphabet = minuscules + majuscules;
+            }
+            else if (choixAlphabet == 3 )
+            {
+                alphabet = minuscules + majuscules + chiffres;
+            }
+            else
+            {
+                alphabet = minuscules + majuscules + chiffres + caracteresSpeciaux;
+            }
+
+           int longueurAlphabet = alphabet.Length;
+
+            for(int j = 0; j < NOMBRE_MDP; j++) // Boucle selon le nombre de MDP choisi dans NOMBRE_MDP.
+            {
+                motDePasse = "";
+                for (int i = 0; i < longueurMotDePasse; i++)
+                {
+                    int index = rand.Next(0, longueurAlphabet);
+                    motDePasse += alphabet[index];
+                }
+                Console.WriteLine($"Votre mot de passe : {motDePasse}");
+            }
+
+        }
+    }
+}
